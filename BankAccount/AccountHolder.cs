be92664d6218510d09bc84cf74f9048930fa4b96
adarsh.k <adarsh.k@TL308>
2022-06-26 11:40:33 +0530

    Add project files.

---
+++ BankAccount/AccountHolder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+    public class AccountHolder
+    {
+        string date = DateTime.UtcNow.ToString("ddMMyyyy");
+        string bankID = "XYZ" + date;
+        Account naman = new Account("Naman", date, bankID, "NAM" + date, "Account Holder", "12345678");
+    }
+}
---
+++ BankAccount/Program.cs
@@ -0,0 +1,52 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using Newtonsoft.Json;
+
+namespace BankAccount // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // Take credentials from user
+            // Read banks.json file and take the first bank
+            // user = bank.VerifyUser(userNmae, password)
+            // if user is NULL > user doesn't exist. Please try again!!
+            // if user is account holder > display operations related to account holder
+            // if user is staff > display operations related to the staff
+        }
+
+        public static Account loginMember()
+        {
+            Console.WriteLine("Hi there !!");
+            Console.WriteLine("Welcome to XYZ Bank");
+            Console.WriteLine("Enter your login credentials to login:");
+
+            Console.WriteLine("Enter your account ID:");
+            String userName = Console.ReadLine();
+            Console.WriteLine("Enter the password:");
+            String password = Console.ReadLine();
+
+            string bankJsonPath = @"C:\Work\Training\DotNet\BankAccount\BankAccount\Data\Banks.json";
+            string bankJsonString = System.IO.File.ReadAllText(bankJsonPath);
+            var list = JsonConvert.DeserializeObject<List<Bank>>(bankJsonString);
+            var first_bank = list.First();
+
+
+            
+            // var first_user = accountList.First();            
+
+
+            Account newUser = first_bank.VerifyUser(userName, password);
+            Console.ReadLine();
+
+            return null;
+            
+        }
+
+        if (newUser.Role.Equals("customer"))
+            {
+                Console.WriteLine(
+            }
+}
+}
